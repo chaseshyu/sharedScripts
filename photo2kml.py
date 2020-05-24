bec0e882b703c5a2f947002fb1146e5dc2d54b4e
Chase J. Shyu <iamhemry@gmail.com>
2020-05-24 09:39:47 +0800

    more styling

--- photo2kml.py
+++ photo2kml.py
@@ -8,12 +8,20 @@
 #  Created by Chase J. Shyu on May 23rd, 2020.
 #  GitHub: https://github.com/chaseshyu/sharedScripts
 import sys, time, glob, math, lxml
-from pykml.factory import KML_ElementMaker as KML
 from PIL import Image
+from pykml.factory import KML_ElementMaker as KML
 
+# name of kml file and layer
 trip_name = "Good Trip 2020"
+
 # choose icon url from http://kml4earth.appspot.com/icons.html
-icon_url = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle_highlight.png'
+icon_url = 'http://maps.google.com/mapfiles/kml/paddle/wht-blank.png'
+
+# x, y fraction for icon alignment
+icon_align = [0.5, 0.0]
+
+# ARGB [transparency, blue, green, red]
+icon_color = [200,211,100,148] # only valid in Google Earth
 
 # choose tags
 meta_tags = ['GPSInfo','DateTime','Model']
@@ -21,9 +29,15 @@ meta_tags = ['GPSInfo','DateTime','Model']
 # Standard Exif Tags from https://www.exiv2.org/tags.html
 tag_dict = {'GPSInfo': 34853, 'DateTime':306, 'Model':272}
 
+# for convertion of coordinate
+orient = {'N':1.,'E':1.,'S':-1.,'W':-1.}
+
+# decimal - hex convertion
+color_argb = "".join('%0*x'%(2,c) for c in icon_color)
+
 def exiftool(files,tag_inds):
-  metadata = []
   inc = False
+  metadata = []
   for imagename in files:
     try:
       image = Image.open(imagename)
@@ -34,95 +48,107 @@ def exiftool(files,tag_inds):
     exifdata = image.getexif()
     try:
       coord = exifdata[tag_inds[0]]
-      preci = int(math.log10(max(coord[2][1][1]*60.,coord[2][2][1]*3600.)))
-      lat = round(sum([l[0]/(l[1]*60.**i) for i,l in enumerate(coord[2])]),preci)
-      lon = round(sum([l[0]/(l[1]*60.**i) for i,l in enumerate(coord[4])]),preci)
+      lat = sum([l[0]/(l[1]*60.**i) for i,l in enumerate(coord[2])])
+      lon = sum([l[0]/(l[1]*60.**i) for i,l in enumerate(coord[4])])
     except KeyError:
-      print('Warning: %s no GPSInfo. Skipped.' % imagename)
+      print('Warning: %s no GPSInfo and skipped.' % imagename)
       continue
 
     if lat > 90. or lon > 180.:
       sys.exit('Error: %s GPSInfo out of range. lon: %f lat: %f' % (imagename,lon,lat))
 
-    if coord[1] == 'S': lat = -lat
-    if coord[3] == 'W': lon = -lon
+    preci = int(math.log10(max(coord[2][1][1]*60.,coord[2][2][1]*3600.)))
+    lat = round(lat*orient[coord[1]],preci)
+    lon = round(lon*orient[coord[3]],preci)
 
     try:
       datetime = exifdata[tag_inds[1]]
     except KeyError:
-      print('Warning: %s no DateTime. Replace by 1900:01:01 00:00:01.' % imagename)
+      print('Warning: %s no DateTime and replace by "1900:01:01 00:00:01".' % imagename)
       datetime = '1900:01:01 00:00:01'
 
     try:
       model = exifdata[tag_inds[2]]
     except KeyError:
-      print('Warning: %s no Model. Replaced by N/A.' % imagename)
+      print('Warning: %s no Model and replaced by "N/A".' % imagename)
       model = 'N/A'
 
     metadata.append([imagename, lon, lat, datetime, model])
   return metadata, inc
 
-def photo2kml(files,tags):
-  tag_inds = [tag_dict[t] for t in tags]
+def photo2kml(files):
+  tag_inds = [tag_dict[t] for t in meta_tags]
   metadata, inc = exiftool(files,tag_inds)
+  # sorded by datetime
+  metadata = sorted(metadata,key=lambda l:l[3])
+  ndata = len(metadata)
+
   if not inc:
     sys.exit('Error: File not found.')
-  elif not len(metadata):
+  elif not ndata:
     sys.exit('Error: GPSInfo not found in file.')
-
-  time_list = [time.strptime(m[3], "%Y:%m:%d %H:%M:%S") for m in metadata]
-  sort_index = sorted(range(len(time_list)), key=lambda k: time_list[k])
 
   kml_doc = KML.kml(
       KML.Document(
           KML.name(trip_name),
           KML.Style(
               KML.IconStyle(
-                  KML.scale(1.),
+                  KML.color(color_argb), # only valid in Google Earth
+                  KML.scale(1.),         # only valid in Google Earth
                   KML.Icon(
                       KML.href(icon_url)
                   ),
+                  KML.hotSpot(
+                    x='0.5',
+                    y='0.0',
+                    xunits="fraction",
+                    yunits="fraction")
               ),
               id='icon_style',
           )
       )
   )
 
-  for i in sort_index:
-    meta = metadata[i]
-    meta[0] = meta[0].replace('./','')
-    coord_text = '%f, %s' % (meta[1],meta[2])
+  # append location one by one
+  for meta in metadata:
 
+    msg = 'Photo location.'
+    #msg += '\nTrip: %s' % trip_name
+    #msg += '\nTime: %s' % meta[3]
+    #msg += '\nModel: %s' % meta[4]
+    #msg += '\nName: %s' % meta[0]
     kml_doc.Document.append(
       KML.Placemark(
+        # use datetime as marker name
         KML.name(meta[3]),
-        KML.styleUrl('#{0}'.format('icon_style')),
-        KML.description('Photo location'),
-        KML.Point(KML.coordinates(coord_text)),
+        KML.styleUrl('#%s' % 'icon_style'),
+        KML.description(msg),
+        KML.Point(KML.coordinates('%f, %f' % (meta[1],meta[2]))),
+        # ExtendedData only valid in Google Map
         KML.ExtendedData(
           KML.Data(KML.value(trip_name),name='Trip'),
           KML.Data(KML.value(meta[3]),name='Time'),
           KML.Data(KML.value(meta[4]),name='Model'),
-          KML.Data(KML.value(meta[0]),name='Name'),
+          KML.Data(KML.value(meta[0].replace('./','')),name='Name'),
         )
       )
     )
-  return kml_doc, len(sort_index)
+  return kml_doc, ndata
 
 def main():
   path = './'
   if len(sys.argv) > 1:
     path = sys.argv[1] + '/'
 
+  # get all file name in folder
   files = glob.glob(path + '*')
-
-  kml_doc, inc = photo2kml(files,meta_tags)
+  kml_doc, inc = photo2kml(files)
 
   with open(trip_name+'.kml','w') as f:
-    #print(etree.tostring(kml_doc, pretty_print=True).decode('UTF-8'))
+    #print(lxml.etree.tostring(kml_doc, pretty_print=True).decode('UTF-8'))
     f.write(lxml.etree.tostring(kml_doc, pretty_print=True).decode('UTF-8'))
 
-  print('photo2kml convertion finished: %d position(s).\n' % inc)
+  print('photo2kml convertion finished: %d location(s).\n' % inc)
 
 if __name__ == '__main__':
   main()
